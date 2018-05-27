7f77ae9137c18eb701915def2e7478a354687bee
sho kisaragi <sho@miko.local>
2018-05-27 09:50:28 +0900

    customized for editor and heroku

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
+const models = require('../models');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  models.User.findAll({
+    limit: 10
+  }).then(function(users) {
+    res.render('users', {
+      title: 'Node.js/Express入門: CRUD サンプル',
+      users: users
+    });
+  });  
+});
+
+router.post('/create', function(req, res) {
+  models.User.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email
+  }).then(function() {
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
